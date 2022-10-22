71c90a245f45e52eec74c479d9d37cfc08fd211d
Thair <110577068+Thair-i@users.noreply.github.com>
2022-10-22 13:38:39 -0700

    add missing data

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -6,15 +6,6 @@ function renderLicenseBadge(data) {
   if (licenseType === "MIT") {
     licenseString = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
   };
-  if (licenseType === "GNU General Public License 2.0") {
-    licenseString = `![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)`
-  };
-  if (licenseType === "Apache License 2.0") {
-     licenseString = `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`
-    };
-  if (licenseType === "GNU General Public License 3.0") {
-      licenseString = `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
-    };
   return licenseString
   };
 
@@ -36,9 +27,14 @@ function renderLicenseLink(license) {
 // Create a function to generate markdown for README
 function generateMarkdowns(data) {
   return `# ${data.title}
+  ${data.user} ${data.email} ${data.description} ${data.install} ${data.contribution}
+  ${data.usage} ${data.test}
   ${renderLicenseBadge(data.license)}
-`;
+
+
+
   
+`;
 }
 
 module.exports = { generateMarkdowns };
